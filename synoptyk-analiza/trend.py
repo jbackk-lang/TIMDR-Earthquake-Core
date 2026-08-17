b2801a600846d26c146ef4657bcf6b9bc8afbf44
Jacek Kielich <jbackk@gmail.com>
2026-08-17 03:13:16 +0200

    Delete synoptyk-analiza/trend.py

--- synoptyk-analiza/trend.py
+++
@@ -1,7 +0,0 @@
-def trend(df):
-    return {
-        "temp": df["temp"].mean(),
-        "precip": df["precip"].mean(),
-        "wind": df["wind"].mean(),
-        "pressure": df["pressure"].mean()
-    }
